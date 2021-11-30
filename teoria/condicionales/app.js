3468d86315dd46bc06883816a6bce056b96f9331
Matias Benary <matiasbenary@gmai.com>
2021-11-30 18:39:33 -0300

    :pencil: Clase 27/11

--- teoria/condicionales/app.js
+++ teoria/condicionales/app.js
@@ -52,3 +52,14 @@ const obtenerMsj= (nombre,edad)=>{
   return `hola ${nombre} tengo ${edad}`
   
 }
+
+
+// esVocal(letra)
+// Crear una función esVocal que tome como argumento un string letra y devuelva true si letra es una vocal o false si no lo es.
+
+const esVocal = (letra)=>{
+  return (letra==="a" || letra==="e"|| letra==="i"|| letra==="o" || letra==="u")
+}
+
+esVocal('a') // true
+esVocal('n') // false
