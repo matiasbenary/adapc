954acc19441c856da4c54d2df2e26b020e900b1e
Matias Benary <matiasbenary@gmai.com>
2021-12-02 23:54:46 -0300

    :pencil: Clase 02/12

--- teoria/array/01-base/resolucion/resoluciones.js
+++ teoria/array/01-base/resolucion/resoluciones.js
@@ -41,13 +41,6 @@ console.log(biblioteca[0][0]);
 biblioteca.push(['Harry Potter 2', 234]);
 console.log(biblioteca);
 
-
-
-
-
-
-
-
 console.log(biblioteca);
 
 for (let index = 0; index < biblioteca.length; index++) {
@@ -65,7 +58,6 @@ for (const indice in biblioteca) {
   console.log(indice);
   const libro = biblioteca[indice];
   console.log(libro);
-  
 }
 /**
  *
@@ -112,12 +104,10 @@ const discoNevermind = [
   'On a Plain',
   'Endless, Nameless',
 ];
-discoNevermind[3] = 'Breed'
-discoNevermind[9] = 'Stay Away'
-
+discoNevermind[3] = 'Breed';
+discoNevermind[9] = 'Stay Away';
 
 console.log(discoNevermind);
-
 
 /*
 ## Ejercicio Array 004
@@ -128,11 +118,20 @@ console.log(discoNevermind);
 - Mostrar en consola la cantidad de etiquetas guardadas en el array
 */
 
-const etiquetasHtml = ["Dody","Main","Header","Div","Footer","p","a","h1","h2"];
+const etiquetasHtml = [
+  'Dody',
+  'Main',
+  'Header',
+  'Div',
+  'Footer',
+  'p',
+  'a',
+  'h1',
+  'h2',
+];
 console.log(etiquetasHtml[1].toUpperCase());
 console.log(etiquetasHtml[4].toLocaleLowerCase());
 console.log(etiquetasHtml.length);
-
 
 /*
 ## Ejercicio Array 006
@@ -150,25 +149,26 @@ const playlistFoo = ["Everlong", "The Pretender", "Learn to Fly"];
 ```
  */
 
-
 const playlistNirvana = [
-  "Smells Like Teen Spirit",
-  "Come As You Are",
-  "Heart-Shaped Box",
-  "Lithium",
+  'Smells Like Teen Spirit',
+  'Come As You Are',
+  'Heart-Shaped Box',
+  'Lithium',
 ];
-const playlistFoo = ["Everlong", "The Pretender", "Learn to Fly"];
+const playlistFoo = ['Everlong', 'The Pretender', 'Learn to Fly'];
 
-if(playlistNirvana.length > playlistFoo.length){
-  console.log("La playlist de Nirvana tiene más canciones")
-}else{
-  console.log("La playlist de Foo Fighters tiene más canciones")
+if (playlistNirvana.length > playlistFoo.length) {
+  console.log('La playlist de Nirvana tiene más canciones');
+} else {
+  console.log('La playlist de Foo Fighters tiene más canciones');
 }
 
-const resultadoPlaylist = playlistNirvana.length > playlistFoo.length?"La playlist de Nirvana tiene más canciones":"La playlist de Foo Fighters tiene más canciones"
+const resultadoPlaylist =
+  playlistNirvana.length > playlistFoo.length
+    ? 'La playlist de Nirvana tiene más canciones'
+    : 'La playlist de Foo Fighters tiene más canciones';
 
 console.log(resultadoPlaylist);
-
 
 /*
 ## Ejercicio Array 007
@@ -181,8 +181,8 @@ const frutas = ["Manzana", "Banana"];
 
 
 */
-const fruta = "Pera"
-const frutas = ["Manzana", "Banana"];
+const fruta = 'Pera';
+const frutas = ['Manzana', 'Banana'];
 frutas.shift();
 frutas.unshift(fruta);
 console.log(frutas);
@@ -217,16 +217,27 @@ console.log(frutas);
 
 
 */
-const concreteAndGold = []
-concreteAndGold.push("T-shirt","Run","Make it Right", "The Sky Is a Neighborhood", "La Dee Da", "Dirty Water", "Arrows", "Happy Ever After (Zero Hour)", "Sunday Rain", "The Line", "Concrete and Gold") 
+const concreteAndGold = [];
+concreteAndGold.push(
+  'T-shirt',
+  'Run',
+  'Make it Right',
+  'The Sky Is a Neighborhood',
+  'La Dee Da',
+  'Dirty Water',
+  'Arrows',
+  'Happy Ever After (Zero Hour)',
+  'Sunday Rain',
+  'The Line',
+  'Concrete and Gold'
+);
 
 console.log(concreteAndGold.shift());
 console.log(concreteAndGold.pop());
 
-concreteAndGold.unshift("T-Shirt")
-concreteAndGold.push("Concrete and Gold")
+concreteAndGold.unshift('T-Shirt');
+concreteAndGold.push('Concrete and Gold');
 console.log(concreteAndGold);
-
 
 /*
 ## Ejercicio Array 014
@@ -251,12 +262,16 @@ console.log(indiceOnAPlain); // deberia mostrar -1
 
 */
 
-
-const playlist = ['Smells Like Teen Spirit', 'Come As You Are', 'Heart-Shaped Box', 'Lithium']
+const playlist = [
+  'Smells Like Teen Spirit',
+  'Come As You Are',
+  'Heart-Shaped Box',
+  'Lithium',
+];
 
 console.log(playlist[1]);
 
-const buscarIndiceCancion = (nombre)=>{
+const buscarIndiceCancion = (nombre) => {
   //nombre => "Lithium"
   for (const i in playlist) {
     // i = 0
@@ -265,19 +280,18 @@ const buscarIndiceCancion = (nombre)=>{
     //if('Smells Like Teen Spirit' == "Lithium")
     // 'Smells Like Teen Spirit' == "Lithium" => false
     // if(false)
-    if(cancion == nombre){
-      return i
+    if (cancion == nombre) {
+      return i;
     }
   }
-  return -1
-}
+  return -1;
+};
 
+console.log(buscarIndiceCancion('Lithium'));
+console.log(buscarIndiceCancion('On a Plain'));
 
-console.log(buscarIndiceCancion("Lithium")); 
-console.log(buscarIndiceCancion("On a Plain")); 
-
-console.log(playlist.indexOf("Lithium")); 
-console.log(playlist.indexOf("On a Plain")); 
+console.log(playlist.indexOf('Lithium'));
+console.log(playlist.indexOf('On a Plain'));
 
 /*
 ## Ejercicio Array 017
@@ -306,32 +320,32 @@ console.log(nombresSeparadosPorGuion);
 */
 
 const womenInTech = [
-  "Ada Lovelace",
-  "Hedy Lamarr",
-  "Grace Hopper",
-  "Radia Perlman",
-  "Janie Tsao",
-  "Sheryl Sandberg",
-  "Susan Kare",
-  "Parisa Tabriz",
+  'Ada Lovelace',
+  'Hedy Lamarr',
+  'Grace Hopper',
+  'Radia Perlman',
+  'Janie Tsao',
+  'Sheryl Sandberg',
+  'Susan Kare',
+  'Parisa Tabriz',
 ];
-let nombresSeparadosPorGuion = "";
+let nombresSeparadosPorGuion = '';
 
 for (const woman of womenInTech) {
-  nombresSeparadosPorGuion +=    woman + " - "
+  nombresSeparadosPorGuion += woman + ' - ';
 }
 
-nombresSeparadosPorGuion = nombresSeparadosPorGuion.slice(0,-3)
+nombresSeparadosPorGuion = nombresSeparadosPorGuion.slice(0, -3);
 
 console.log(nombresSeparadosPorGuion);
 
-nombresSeparadosPorGuion = womenInTech.toString().replaceAll(',',' - ');
+// nombresSeparadosPorGuion = womenInTech.toString().replaceAll(',',' - ');
 
 // aca va la solucion
 
-console.log(nombresSeparadosPorGuion); 
+console.log(nombresSeparadosPorGuion);
 
-console.log(womenInTech.join(" - "))
+console.log(womenInTech.join(' - '));
 
 /*
 ## Ejercicio Array 019
@@ -354,18 +368,226 @@ console.log(notaFinal);
 
 const notasDeTPs = [4, 7, 8, 5, 10];
 let notaFinal = 0;
-let suma = 0
+let suma = 0;
 for (const nota of notasDeTPs) {
-  suma += nota 
+  suma += nota;
 }
-const division = notasDeTPs.length
-notaFinal = suma/division
+const division = notasDeTPs.length;
+notaFinal = suma / division;
 console.log(notaFinal);
 // deberia mostrar 6.8
 
-notaFinal =0
+notaFinal = 0;
 for (const nota of notasDeTPs) {
-  notaFinal += nota/ notasDeTPs.length
+  notaFinal += nota / notasDeTPs.length;
 }
 
 console.log(notaFinal);
+
+const saludar = (nombre) => {
+  nombre = nombre || 'Matias';
+  console.log(nombre);
+};
+const testCircuitoCorto = false || 'Test';
+console.log(testCircuitoCorto);
+saludar('');
+
+console.log(womenInTech.pop().toUpperCase());
+
+let i = 0;
+while (i < womenInTech.length) {
+  console.log(womenInTech[i]);
+  i++;
+}
+
+let j = 0;
+do {
+  console.log(womenInTech[j]);
+  j++;
+} while (j < womenInTech.length);
+
+for (let index = 0; index < womenInTech.length; index++) {
+  const element = womenInTech[index];
+  console.log(element);
+}
+
+for (const woman of womenInTech) {
+  console.log(woman);
+}
+
+// Definí una función sonIguales(a, b) que reciba como argumentos dos arrays a y b y devuelva si ambos arrays tienen los mismos valores en la misma posición.
+const sonIguales = (a, b) => {
+  //comparo longitud si son difentes retorno falso
+  if (a.length != b.length) {
+    return false;
+  }
+
+  // for (let index = 0; index < a.length; index++) {
+  for (let index in a) {
+    if (a[index] !== b[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(sonIguales([10, 25, 6, 33, 48, 105], [10, 25, 6, 33, 48, 105]));
+console.log(sonIguales([10, 25, 6, 33, 48, 105], [10, 25, 6, 33]));
+console.log(sonIguales([10, 25, 6, 33], [10, 25, 6, 33, 48, 105]));
+console.log(sonIguales([10, 34, 6, 33, 48, 105], [10, 25, 6, 33, 48, 105]));
+console.log(sonIguales([10, 25, 6, 33], [10, 25, 6, 33, 48, 105]));
+
+// Definí una función tieneBloque que tome por parámetro un array y devuelva si dicho array tiene un bloque de 3 o más ítems consecutivos idénticos.
+
+const tieneBloque1 = (array) => {
+  let valorAnterior = null;
+  let contadorDeRepetidos = 1;
+  for (const index in array) {
+    // for (let index = 0; index < array.length; index++) {
+    const valorActual = array[index];
+    console.log(valorAnterior, valorActual);
+  
+    if (valorActual == valorAnterior) {
+      contadorDeRepetidos++;
+    } else {
+      contadorDeRepetidos = 1;
+    }
+
+    if (contadorDeRepetidos == 3) {
+      return true;
+    }
+
+    valorAnterior = valorActual;
+  }
+  return false;
+};
+
+console.log(tieneBloque1([1, 2, 3]));
+console.log(tieneBloque1([2, 1, 1, 1, 2, 3]));
+console.log(tieneBloque1([1, 2, 3, 3, 3]));
+console.log(tieneBloque1([1, 2, 3, 3, 3, 8]));
+console.log(tieneBloque1([1, 2, 2, 3, 3, 4]));
+console.log(tieneBloque1([1, 2, 3, 3, 3, 8, 4, 5, 5, 5, 6, 7]));
+
+const tieneBloque = (array) => {
+  let valorAnterior = null;
+  let contadorDeRepetidos = 1;
+  for (const valorActual of array) {
+    // for (let index = 0; index < array.length; index++) {
+    // const valorActual = element;
+    console.log(valorAnterior, valorActual);
+    //null 2 0 1
+    //2 1 0 1
+    //1 1 1 2
+    //1 1 2 3
+    //1 2 0 1
+    //2 3 0 1
+    // if(valorActual == valorAnterior) {
+    //   contadorDeRepetidos++
+    // }else{
+    //   contadorDeRepetidos = 1
+    // }
+    contadorDeRepetidos =
+      valorActual == valorAnterior ? contadorDeRepetidos + 1 : 1;
+
+    if (contadorDeRepetidos == 3) {
+      return true;
+    }
+
+    valorAnterior = valorActual;
+  }
+  return false;
+};
+
+console.log(tieneBloque([1, 2, 3]));
+console.log(tieneBloque([2, 1, 1, 1, 2, 3]));
+console.log(tieneBloque([1, 2, 3, 3, 3]));
+console.log(tieneBloque([1, 2, 3, 3, 3, 8]));
+console.log(tieneBloque([1, 2, 2, 3, 3, 4]));
+console.log(tieneBloque([1, 2, 3, 3, 3, 8, 4, 5, 5, 5, 6, 7]));
+
+const tieneBloque2 = (array) => {
+  for (const index in array) {
+    if (
+      array[index] == array[Number(index) + 1] &&
+      array[index] == array[Number(index) + 2]
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(tieneBloque2([1, 2, 3]));
+console.log(tieneBloque2([2, 1, 1, 1, 2, 3]));
+console.log(tieneBloque2([1, 2, 3, 3, 3]));
+console.log(tieneBloque2([1, 2, 3, 3, 3, 8]));
+console.log(tieneBloque2([1, 2, 2, 3, 3, 4]));
+console.log(tieneBloque2([1, 2, 3, 3, 3, 8, 4, 5, 5, 5, 6, 7]));
+
+// Definí una función tieneBloqueHorizontal que reciba como argumento un array 2d matriz y devuelva si dicha matriz tiene un bloque horizontal de 3 o más ítems consecutivos idénticos.
+
+const tieneBloqueHorizontal = (matrix) => {
+  for (const array of matrix) {
+    if(tieneBloque(array)) {
+      return true
+    }
+  }
+  return false;
+};
+console.log(
+  tieneBloqueHorizontal([
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ])
+);
+
+console.log(
+  tieneBloqueHorizontal([
+    [1, 2, 3, 4],
+    [1, 2, 2, 2],
+    [1, 2, 4, 5],
+  ])
+);
+
+//Definí una función tieneBloqueVertical que tome una matriz y devuelva si la misma tiene un bloque vertical  de 3 o más ítems consecutivos idénticos .
+
+const girarMatriz = (matrix) =>{
+  const lengthX = matrix[0].length
+  const lengthY = matrix.length
+  const matrixGirada = []
+  for (const indexX in matrix[0]) {
+    const matrixX = [];
+    for (const indexY in matrix) {
+      // if(indexY > lengthX || indexX > lengthY) {
+      //   continue
+      // }
+      matrixX.push(matrix[indexY][indexX])
+    }
+    matrixGirada.push(matrixX);
+  }
+  return matrixGirada;
+}
+
+const tieneBloqueVertical = (matrix) =>{
+  const matrixGirada = girarMatriz(matrix)
+  console.log({matrix,matrixGirada});
+
+  for (const array of matrixGirada) {
+    if(tieneBloque(array)) {
+      return true
+    }
+  }
+
+}
+console.log(tieneBloqueVertical([
+  [4, 2, 3],
+  [1, 5, 3],
+  [1, 2, 3],
+]));
+
+console.log(tieneBloqueVertical([
+  [1, 2, 3, 4],
+  [7, 2, 9, 2],
+  [1, 8, 4, 5],
+]) );
